d4126331c43f0942a5000274dd1e7e4bc4351447
vickykumar1001 <vicky9060993097@gmail.com>
2024-11-07 15:53:54 +0530

    updated Register Screen - connected with backend

--- src/screens/ResigterScreen.js
+++ src/screens/ResigterScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-import { View, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, StyleSheet, TouchableOpacity, ScrollView, KeyboardAvoidingView, Platform } from 'react-native'
 import { Text } from 'react-native-paper'
+import axios from 'axios';
 import { Dropdown } from 'react-native-element-dropdown';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import Background from '../components/Background'
@@ -17,119 +18,160 @@ import { roleValidator } from '../helpers/roleValidator'
 import { mobileValidator } from '../helpers/mobileValidator'
 
 export default function RegisterScreen({ navigation }) {
-  const [name, setName] = useState({ value: '', error: '' })
+  const [firstName, setFirstName] = useState({ value: '', error: '' })
+  const [lastName, setLastName] = useState({ value: '', error: '' })
   const [email, setEmail] = useState({ value: '', error: '' })
   const [password, setPassword] = useState({ value: '', error: '' })
   const [mobile, setMobile] = useState({ value: '', error: '' })
   const [role, setRole] = useState({ value: '', error: '' })
 
   const data = [
-    { label: 'Farmer', value: 'farmer' },
-    { label: 'Buyer', value: 'buyer' },
+    { label: 'Farmer', value: 'Farmer' },
+    { label: 'Seller', value: 'Seller' },
   ];
+
   const onSignUpPressed = () => {
-    const nameError = nameValidator(name.value)
+    const firstNameError = nameValidator(firstName.value)
+    const lastNameError = nameValidator(lastName.value)
     const emailError = emailValidator(email.value)
     const passwordError = passwordValidator(password.value)
     const mobileError = mobileValidator(mobile.value)
     const roleError = roleValidator(role.value)
-    if (emailError || passwordError || nameError || roleError) {
-      setName({ ...name, error: nameError })
+    if (emailError || passwordError || firstNameError || lastNameError || roleError) {
+      setFirstName({ ...firstName, error: firstNameError })
+      setLastName({ ...lastName, error: lastNameError })
       setEmail({ ...email, error: emailError })
       setPassword({ ...password, error: passwordError })
       setRole({ ...role, error: roleError })
       setMobile({ ...mobile, error: mobileError })
       return
     }
-    navigation.navigate("VerifyEmailonRegister")
+    const userData = {
+      firstName: firstName.value,
+      lastName: lastName.value,
+      email: email.value,
+      password: password.value,
+      mobile: mobile.value,
+      role: role.value,
+    }
+    console.log("Signup")
+    axios
+      .post("http://192.168.0.106:5454/auth/signup", userData)
+      .then((response) => {
+        console.log(response.data.message);
+        navigation.navigate('VerifyEmailonRegister', { email: email.value });
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
   }
 
   return (
     <Background>
       <BackButton goBack={navigation.goBack} />
-
-      <Logo />
-      <Header>Create Account</Header>
-      <Dropdown
-        style={[!role.error ? styles.dropdown : styles.dropdownError]}
-        placeholderStyle={!role.error ? styles.placeholderStyle : styles.placeholderStyleError}
-        selectedTextStyle={styles.selectedTextStyle}
-        inputSearchStyle={styles.inputSearchStyle}
-        iconStyle={styles.iconStyle}
-        data={data}
-        maxHeight={300}
-        labelField="label"
-        valueField="value"
-        returnKeyType="next"
-        placeholder={'Select Your Role'}
-        value={role.value}
-        onChange={item => {
-          setRole({ value: item.value, error: '' })
-        }
-        }
-        error={!!role.error}
-        errorText={role.error}
-      />
-      <View style={styles.container}>
-        {role.error ? <Text style={styles.error}>{role.error}</Text> : null}
-      </View>
-      <TextInput
-        label="Name"
-        returnKeyType="next"
-        value={name.value}
-        onChangeText={(text) => setName({ value: text, error: '' })}
-        error={!!name.error}
-        errorText={name.error}
-      />
-      <TextInput
-        label="Email"
-        returnKeyType="next"
-        value={email.value}
-        onChangeText={(text) => setEmail({ value: text, error: '' })}
-        error={!!email.error}
-        errorText={email.error}
-        autoCapitalize="none"
-        autoCompleteType="email"
-        textContentType="emailAddress"
-        keyboardType="email-address"
-      />
-      <TextInput
-        label="Mobile"
-        returnKeyType="next"
-        value={mobile.value}
-        onChangeText={(text) => setMobile({ value: text, error: '' })}
-        error={!!mobile.error}
-        errorText={mobile.error}
-        // textContentType="numeric"
-        keyboardType="numeric"
-      />
-      <TextInput
-        label="Password"
-        returnKeyType="done"
-        value={password.value}
-        onChangeText={(text) => setPassword({ value: text, error: '' })}
-        error={!!password.error}
-        errorText={password.error}
-        secureTextEntry
-      />
-      <Button
-        mode="contained"
-        onPress={onSignUpPressed}
-        style={{ marginTop: 24 }}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={{ flex: 1 }}
       >
-        Sign Up
-      </Button>
-      <View style={styles.row}>
-        <Text>Already have an account? </Text>
-        <TouchableOpacity onPress={() => navigation.replace('LoginScreen')}>
-          <Text style={styles.link}>Login</Text>
-        </TouchableOpacity>
-      </View>
+        <ScrollView contentContainerStyle={styles.scrollContainer} keyboardShouldPersistTaps="handled" showsVerticalScrollIndicator={false}>
+
+          <Logo />
+          <Header>Create Account</Header>
+          <Dropdown
+            style={[!role.error ? styles.dropdown : styles.dropdownError]}
+            placeholderStyle={!role.error ? styles.placeholderStyle : styles.placeholderStyleError}
+            selectedTextStyle={styles.selectedTextStyle}
+            inputSearchStyle={styles.inputSearchStyle}
+            iconStyle={styles.iconStyle}
+            data={data}
+            maxHeight={300}
+            labelField="label"
+            valueField="value"
+            returnKeyType="next"
+            placeholder={'Select Your Role'}
+            value={role.value}
+            onChange={item => {
+              setRole({ value: item.value, error: '' })
+            }}
+            error={!!role.error}
+            errorText={role.error}
+          />
+          <View style={styles.container}>
+            {role.error ? <Text style={styles.error}>{role.error}</Text> : null}
+          </View>
+          <TextInput
+            label="First Name"
+            returnKeyType="next"
+            value={firstName.value}
+            onChangeText={(text) => setFirstName({ value: text, error: '' })}
+            error={!!firstName.error}
+            errorText={firstName.error}
+          />
+          <TextInput
+            label="Last Name"
+            returnKeyType="next"
+            value={lastName.value}
+            onChangeText={(text) => setLastName({ value: text, error: '' })}
+            error={!!lastName.error}
+            errorText={lastName.error}
+          />
+          <TextInput
+            label="Email"
+            returnKeyType="next"
+            value={email.value}
+            onChangeText={(text) => setEmail({ value: text, error: '' })}
+            error={!!email.error}
+            errorText={email.error}
+            autoCapitalize="none"
+            autoCompleteType="email"
+            textContentType="emailAddress"
+            keyboardType="email-address"
+          />
+          <TextInput
+            label="Mobile"
+            returnKeyType="next"
+            value={mobile.value}
+            onChangeText={(text) => setMobile({ value: text, error: '' })}
+            error={!!mobile.error}
+            errorText={mobile.error}
+            keyboardType="numeric"
+          />
+          <TextInput
+            label="Password"
+            returnKeyType="done"
+            value={password.value}
+            onChangeText={(text) => setPassword({ value: text, error: '' })}
+            error={!!password.error}
+            errorText={password.error}
+            secureTextEntry
+          />
+          <Button
+            mode="contained"
+            onPress={onSignUpPressed}
+            style={{ marginTop: 24 }}
+          >
+            Sign Up
+          </Button>
+          <View style={styles.row}>
+            <Text>Already have an account? </Text>
+            <TouchableOpacity onPress={() => navigation.replace('LoginScreen')}>
+              <Text style={styles.link}>Login</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </Background>
   )
 }
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    margin: 0,
+    padding: 0,
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
   error: {
     fontSize: 13,
     color: theme.colors.error,
@@ -165,17 +207,6 @@ const styles = StyleSheet.create({
     borderWidth: 1.3,
     borderRadius: 5,
     paddingHorizontal: 10,
-  },
-  icon: {
-    marginRight: 5,
-  },
-  label: {
-    position: 'absolute',
-    left: 22,
-    top: 8,
-    zIndex: 999,
-    paddingHorizontal: 8,
-    fontSize: 14,
   },
   placeholderStyle: {
     fontSize: 16,
